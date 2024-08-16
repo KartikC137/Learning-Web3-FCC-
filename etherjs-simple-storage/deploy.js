aa580b95ad0ee1966c46dc0855905bd4fdd2ef2f
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-08-16 18:12:52 +0530

    Encrypted Key

--- etherjs-simple-storage/deploy.js
+++ etherjs-simple-storage/deploy.js
@@ -24,7 +24,13 @@ async function main() {
   // compile them seperately
 
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  //const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PASS
+  );
+  wallet = await wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",

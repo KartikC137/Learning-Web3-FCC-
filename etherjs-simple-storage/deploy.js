43311fd79cb7a492f845b0d467d5281ff00d6de2
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-08-17 11:14:29 +0530

    Using sepolia testnet to deploy contract

--- etherjs-simple-storage/deploy.js
+++ etherjs-simple-storage/deploy.js
@@ -24,13 +24,15 @@ async function main() {
   // compile them seperately
 
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-  //const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-    encryptedJson,
-    process.env.PASS,
-  )
-  wallet = await wallet.connect(provider)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+
+  // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PASS,
+  // )
+  // wallet = await wallet.connect(provider)
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
@@ -41,12 +43,13 @@ async function main() {
   console.log("Please wait, deploying...")
   const contract = await contractFactory.deploy() //stop here and wait for contract to be deployed, hence await keyword
   await contract.deployTransaction.wait(1)
+  console.log(`Contract Address: ${contract.address}`)
 
   //get number
 
   const currentFavouriteNumber = await contract.getNumber()
   console.log("Current Favourite Number: " + currentFavouriteNumber.toString())
-  const transactionResponse = await contract.store("8")
+  const transactionResponse = await contract.store("9")
   const transactionReceipt = await transactionResponse.wait(1)
   const updatedFavouriteNumber = await contract.getNumber()
   console.log("Updated Favourite Number: " + updatedFavouriteNumber.toString())

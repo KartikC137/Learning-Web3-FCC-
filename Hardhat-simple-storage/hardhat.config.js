989bac4301ff1277bb35b98f72999fc2ebe8fcae
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-08-18 15:01:30 +0530

    deployed on sepolia

--- Hardhat-simple-storage/hardhat.config.js
+++ Hardhat-simple-storage/hardhat.config.js
@@ -1,11 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const sepoliaRPC = process.env.SEPOLIA_RPC_URL
+const pKEY = process.env.PRIVATE_KEY
+
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     sepolia: {
-      
+      url: sepoliaRPC,
+      accounts: [pKEY],
+      chainId: 11155111,
     },
   },
   solidity: "0.8.24",

07bd4c44658cabe4b43f9bfc83c1aaa0b0e492b0
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-08-21 22:41:14 +0530

    deployement on hardhat local network

--- Hardhat-simple-storage/hardhat.config.js
+++ Hardhat-simple-storage/hardhat.config.js
@@ -16,6 +16,10 @@ module.exports = {
             accounts: [pKEY],
             chainId: 11155111,
         },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            chainId: 31337,
+        },
     },
 
     solidity: "0.8.24",

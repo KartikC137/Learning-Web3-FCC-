130a8ff2acdad11904d6151505b7ccfcafc945e1
KartikC137 <119693639+KartikC137@users.noreply.github.com>
2024-09-09 23:47:28 +0530

    Completed first Smart Contract!

---
+++ README.md
@@ -0,0 +1,3 @@
+## Lesson 7
+
+Developed on Clone of [PatrickAlphaC hardhat-fund-me-fcc](https://github.com/PatrickAlphaC/hardhat-fund-me-fcc)
---
+++ hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -0,0 +1,43 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+require("dotenv").config()
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //if chainID is X use address Y format
+
+    //if the contract doesn't exist, we deploy a minimal version of it for our local testing
+
+    let ethUsdPriceFeedAddress
+    if (chainId == 31337) {
+        //localhost
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        //testnets
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    log(`FundMe deployed at ${fundMe.address}`)
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+}
+
+module.exports.tags = ["all", "fundme"]
